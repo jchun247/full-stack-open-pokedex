be33f7f8d7fc9be2e6edc4f1e7685412088633c3
Justin Chung <jchun247@uwo.ca>
2024-05-03 14:20:48 -0400

    Added pokemon page test

--- tests/e2e-tests/pokedex.spec.js
+++ tests/e2e-tests/pokedex.spec.js
@@ -1,17 +1,22 @@
 const { test, describe, expect, beforeEach } = require("@playwright/test");
 
 describe("Pokedex", () => {
-  // beforeEach(async ({ page }) => {
-  //   await page.goto("http://localhost:8080");
-  // });
+  beforeEach(async ({ page }) => {
+    await page.goto("");
+  });
 
   test("front page can be opened", async ({ page }) => {
-    await page.goto("");
     await expect(page.getByText("ivysaur")).toBeVisible();
     await expect(
       page.getByText(
         "Pokémon and Pokémon character names are trademarks of Nintendo."
       )
     ).toBeVisible();
+  });
+
+  test("pokemon page can be navigated to", async({ page }) => {
+    await page.goto("/pokemon/ivysaur");
+    await expect(page.getByText("speed")).toBeVisible();
+    await expect(page.getByText("chlorophyll")).toBeVisible();
   });
 });
