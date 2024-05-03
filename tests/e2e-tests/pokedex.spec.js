d82e2d5c408c5dcc719f8602ccac8fb3e5ea4d90
Justin Chung <jchun247@uwo.ca>
2024-05-03 14:15:43 -0400

    Add e2e tests to pipeline

---
+++ tests/e2e-tests/pokedex.spec.js
@@ -0,0 +1,17 @@
+const { test, describe, expect, beforeEach } = require("@playwright/test");
+
+describe("Pokedex", () => {
+  // beforeEach(async ({ page }) => {
+  //   await page.goto("http://localhost:8080");
+  // });
+
+  test("front page can be opened", async ({ page }) => {
+    await page.goto("");
+    await expect(page.getByText("ivysaur")).toBeVisible();
+    await expect(
+      page.getByText(
+        "Pokémon and Pokémon character names are trademarks of Nintendo."
+      )
+    ).toBeVisible();
+  });
+});
